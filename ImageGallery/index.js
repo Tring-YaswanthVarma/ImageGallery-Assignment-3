50373ac244ab29d5a417935647fab8cac0a96226
Tringapps-YaswanthVarma <123439743+YaswanthVarma754@users.noreply.github.com>
2023-03-12 00:00:08 +0530

    Add files via upload

---
+++ ImageGallery/index.js
@@ -0,0 +1,23 @@
+let lightbox = document.getElementById('overlay')
+
+
+let imgs = document.querySelectorAll('img')
+let image = document.getElementById('lightImage')
+
+let close = document.getElementById('close')
+
+
+imgs.forEach(img =>
+{
+    img.addEventListener('click', () =>
+    {
+        image.src = img.src
+        lightbox.classList.add('active')
+        document.body.style.overflow = "hidden"
+    })
+})
+
+close.addEventListener('click', () => {
+    lightbox.classList.remove('active')
+    document.body.style.overflow = "visible"
+})
